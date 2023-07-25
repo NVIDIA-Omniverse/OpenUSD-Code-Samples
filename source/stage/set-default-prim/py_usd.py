1c2af702f7b88f63c7e630fe1588b09703786dde
Felix Meyer <fmeyer@nvidia.com>
2023-07-25 18:18:06 +0200

    Ported set-default-prim sample

---
+++ source/stage/set-default-prim/py_usd.py
@@ -0,0 +1,23 @@
+from pxr import Usd, UsdGeom, Sdf
+
+def set_default_prim(stage: Usd.Stage, prim: Usd.Prim):
+    stage.SetDefaultPrim(prim)
+
+
+#############
+# Full Usage
+#############
+# Create new USD stage for this sample
+stage: Usd.Stage = Usd.Stage.CreateInMemory()
+
+# Create an xform which should be set as the default prim
+default_prim: Usd.Prim = UsdGeom.Xform.Define(stage, Sdf.Path("/World")).GetPrim()
+
+# Make the xform the default prim
+set_default_prim(stage, default_prim)
+
+usda = stage.GetRootLayer().ExportToString()
+print(usda)
+
+# Check that the expected default prim was set
+assert stage.GetDefaultPrim() == default_prim
