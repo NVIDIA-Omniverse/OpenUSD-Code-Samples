e576912b6ffd2867137e15c14beafd6a8340d4aa
Felix Meyer <fmeyer@nvidia.com>
2023-07-28 16:56:04 +0200

    Simplified Create References/Payloads sample

--- source/references-payloads/create-payload/py_kit_cmds.py
+++ source/references-payloads/create-payload/py_kit_cmds.py
@@ -5,42 +5,32 @@ import omni.kit.commands
 import omni.usd
 from pxr import Usd, Sdf, UsdGeom
 
-def create_payload(context: omni.usd.UsdContext, prim_path: Sdf.Path, payload_asset_path: str, payload_target_path: Sdf.Path) -> Usd.Prim:
+def create_payload(usd_context: omni.usd.UsdContext, path_to: Sdf.Path, asset_path: str, prim_path: Sdf.Path) -> Usd.Prim:
     omni.kit.commands.execute("CreatePayload",
-        path_to=prim_path, # Prim path for where to create the payload
-        asset_path=payload_asset_path, # The file path to payload. Relative paths are accepted too.
-        prim_path=payload_target_path,
-        usd_context=context
+        usd_context=usd_context,
+        path_to=path_to, # Prim path for where to create the prim with the payload
+        asset_path=asset_path, # The file path to the payload USD. Relative paths are accepted too.
+        prim_path=prim_path # OPTIONAL: Prim path to a prim in the payloaded USD, if not provided the default prim is used
     )
-    return stage.GetPrimAtPath(prim_path)
+    return usd_context.get_stage().GetPrimAtPath(path_to)
 
 
 #############
 # Full Usage
 #############
 
-# Create new USD stage for this sample in OV
+# Get the USD context from kit
 context: omni.usd.UsdContext = omni.usd.get_context()
-success: bool = context.new_stage()
-stage: Usd.Stage = context.get_stage()
-
-# Create and define default prim, so this file can be easily payloadd again
-default_prim = UsdGeom.Xform.Define(stage, Sdf.Path("/World"))
-stage.SetDefaultPrim(default_prim.GetPrim())
 
 # Create and add external payload to specific prim
 payload_prim: Usd.Prim = create_payload(context, Sdf.Path("/World/payload_prim"), "C:/path/to/file.usd", Sdf.Path("/World/some/target"))
 
-# Create and add external payload to default prim
-def_payload_prim: Usd.Prim = create_payload(context, Sdf.Path("/World/payload_default"), "C:/path/to/other/file.usd", Sdf.Path.emptyPath)
-
+# Get the existing USD stage from kit
+stage: Usd.Stage = context.get_stage()
 usda = stage.GetRootLayer().ExportToString()
 print(usda)
 
 # Check that the payload prims were created
-assert success
 assert payload_prim.IsValid()
-assert def_payload_prim.IsValid()
 
 assert payload_prim.GetPrimStack()[0].payloadList.prependedItems[0] == Sdf.Payload(assetPath="file:/C:/path/to/file.usd", primPath=Sdf.Path("/World/some/target"))
-assert def_payload_prim.GetPrimStack()[0].payloadList.prependedItems[0] == Sdf.Payload(assetPath="file:/C:/path/to/other/file.usd")
--- source/references-payloads/create-reference/py_kit_cmds.py
+++ source/references-payloads/create-reference/py_kit_cmds.py
@@ -5,58 +5,32 @@ import omni.kit.commands
 import omni.usd
 from pxr import Usd, Sdf, UsdGeom
 
-def create_int_reference(context: omni.usd.UsdContext, prim_path: Sdf.Path, ref_target_path: Sdf.Path) -> Usd.Prim:
+def create_reference(usd_context: omni.usd.UsdContext, path_to: Sdf.Path, asset_path: str, prim_path: Sdf.Path) -> Usd.Prim:
     omni.kit.commands.execute("CreateReference",
-        path_to=prim_path, # Prim path for where to create the reference
-        prim_path=ref_target_path,
-        usd_context=context
+        usd_context=usd_context,
+        path_to=path_to, # Prim path for where to create the prim with the reference
+        asset_path=asset_path, # The file path to reference. Relative paths are accepted too.
+        prim_path=prim_path # OPTIONAL: Prim path to a prim in the referenced USD, if not provided the default prim is used
     )
-    return stage.GetPrimAtPath(prim_path)
-
-def create_ext_reference(context: omni.usd.UsdContext, prim_path: Sdf.Path, ref_asset_path: str, ref_target_path: Sdf.Path) -> Usd.Prim:
-    omni.kit.commands.execute("CreateReference",
-        path_to=prim_path, # Prim path for where to create the reference
-        asset_path=ref_asset_path, # The file path to reference. Relative paths are accepted too.
-        prim_path=ref_target_path,
-        usd_context=context
-    )
-    return stage.GetPrimAtPath(prim_path)
+    return usd_context.get_stage().GetPrimAtPath(path_to)
 
 
 #############
 # Full Usage
 #############
 
-# Create new USD stage for this sample in OV
+# Get the USD context from kit
 context: omni.usd.UsdContext = omni.usd.get_context()
-success: bool = context.new_stage()
-stage: Usd.Stage = context.get_stage()
-
-# Create and define default prim, so this file can be easily referenced again
-default_prim = UsdGeom.Xform.Define(stage, Sdf.Path("/World"))
-stage.SetDefaultPrim(default_prim.GetPrim())
-
-# Create and add internal reference to specific prim
-intern_target_path: Sdf.Path = Sdf.Path("/World/intern_target")
-target_prim: Usd.Prim = UsdGeom.Xform.Define(stage, intern_target_path).GetPrim()
-int_ref_prim: Usd.Prim = create_int_reference(context, Sdf.Path("/World/int"), intern_target_path)
 
 # Create and add external reference to specific prim
-ext_ref_prim: Usd.Prim = create_ext_reference(context, Sdf.Path("/World/ref_ext"), "C:/path/to/file.usd", Sdf.Path("/World/some/target"))
+ref_prim: Usd.Prim = create_reference(context, Sdf.Path("/World/ref_prim"), "C:/path/to/file.usd", Sdf.Path("/World/some/target"))
 
-# Create and add external reference to default prim
-def_ext_ref_prim: Usd.Prim = create_ext_reference(context, Sdf.Path("/World/ref_ext_default"), "C:/path/to/other/file.usd", Sdf.Path.emptyPath)
-
+# Get the existing USD stage from kit
+stage: Usd.Stage = context.get_stage()
 usda = stage.GetRootLayer().ExportToString()
 print(usda)
 
 # Check that the reference prims were created
-assert success
-assert target_prim.IsValid()
-assert int_ref_prim.IsValid()
-assert ext_ref_prim.IsValid()
-assert def_ext_ref_prim.IsValid()
+assert ref_prim.IsValid()
 
-assert int_ref_prim.GetPrimStack()[0].referenceList.prependedItems[0] == Sdf.Reference(primPath=intern_target_path)
-assert ext_ref_prim.GetPrimStack()[0].referenceList.prependedItems[0] == Sdf.Reference(assetPath="file:/C:/path/to/file.usd", primPath=Sdf.Path("/World/some/target"))
-assert def_ext_ref_prim.GetPrimStack()[0].referenceList.prependedItems[0] == Sdf.Reference(assetPath="file:/C:/path/to/other/file.usd")
+assert ref_prim.GetPrimStack()[0].referenceList.prependedItems[0] == Sdf.Reference(assetPath="file:/C:/path/to/file.usd", primPath=Sdf.Path("/World/some/target"))
