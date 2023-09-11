87edc4bc70fb9cc2ae9ffed3353a468d7842cafe
jborucki <jborucki@nvidia.com>
2023-09-11 14:47:52 -0700

    Issue #31 Adding Usage for Get World Transforms
    Signed-off-by: jborucki <jborucki@nvidia.com>

--- source/transforms/get-world-transforms/py_omni_usd.py
+++ source/transforms/get-world-transforms/py_omni_usd.py
@@ -1,7 +1,11 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-def get_world_transform_xform(prim: Usd.Prim) -> typing.Tuple[Gf.Vec3d, Gf.Rotation, Gf.Vec3d]
+import omni.usd
+from pxr import Usd, Gf
+import typing
+
+def get_world_transform_xform(prim: Usd.Prim) -> typing.Tuple[Gf.Vec3d, Gf.Rotation, Gf.Vec3d]:
     """
     Get the local transformation of a prim using omni.usd.get_world_transform_matrix().
     See https://docs.omniverse.nvidia.com/kit/docs/omni.usd/latest/omni.usd/omni.usd.get_world_transform_matrix.html
@@ -19,3 +23,52 @@ def get_world_transform_xform(prim: Usd.Prim) -> typing.Tuple[Gf.Vec3d, Gf.Rotat
     scale: Gf.Vec3d = Gf.Vec3d(*(v.GetLength() for v in world_transform.ExtractRotationMatrix()))
     return translation, rotation, scale
     
+#############
+# Full Usage
+#############
+
+import omni.kit.commands
+
+# Create an Xform with a Cube Prim as it's Child
+omni.kit.commands.execute('CreatePrimWithDefaultXform',
+	prim_type='Xform',
+	attributes={},
+	select_new_prim=True)
+
+omni.kit.commands.execute('TransformMultiPrimsSRTCpp',
+	count=1,
+	paths=['/World/Xform'],
+	new_translations=[100, 0, 0],
+	new_rotation_eulers=[0, 50 ,0],
+	new_rotation_orders=[0, 1, 2],
+	new_scales=[5, 5, 5],
+	old_translations=[0.0, 0.0, 0.0],
+	old_rotation_eulers=[0.0, 0.0, 0.0],
+	old_rotation_orders=[0, 1, 2],
+	old_scales=[1.0, 1.0, 1.0],
+	usd_context_name='',
+	time_code=0.0)
+
+omni.kit.commands.execute('CreateMeshPrimWithDefaultXform',
+	prim_type='Cube',
+	prim_path='/World/Xform/Cube',
+	select_new_prim=True,
+	prepend_default_prim=False)
+
+omni.kit.commands.execute('TransformMultiPrimsSRTCpp',
+	count=1,
+	paths=['/World/Xform/Cube'],
+	new_translations=[4, 0, 0],
+	new_rotation_eulers=[100, 0 ,0],
+	new_rotation_orders=[0, 1, 2],
+	new_scales=[2, 2, 2],
+	old_translations=[0.0, 0.0, 0.0],
+	old_rotation_eulers=[0.0, 0.0, 0.0],
+	old_rotation_orders=[0, 1, 2],
+	old_scales=[1.0, 1.0, 1.0],
+	usd_context_name='',
+	time_code=0.0)
+
+stage = omni.usd.get_context().get_stage()
+cube_prim = stage.GetPrimAtPath("/World/Xform/Cube")
+transform = get_world_transform_xform(cube_prim)
---
+++ source/transforms/get-world-transforms/usda.md
@@ -0,0 +1,4 @@
+This is an example USDA result from creating a Xform with a Cube as a child.
+``` {literalinclude} usda.usda
+:language: usd
+``` 
