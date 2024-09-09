dda7b1cb854ee3cc8a69442c2bb7bb954d85e10f
Austin Hwang <69058754+apric0ts@users.noreply.github.com>
2024-09-09 17:13:05 -0400

    Issue #17 - Get value of custom metadata field (#71)
    Signed-off-by: apric0ts <austinhwang18@gmail.com>

---
+++ source/properties/get-custom-metadata/py_usd.md
@@ -0,0 +1,10 @@
+Get custom metadata on a Usd Object.
+
+``` {literalinclude} py_usd.py
+:language: py
+```
+
+Get customData metadata on a Usd Object.
+``` {literalinclude} py_usd_var1.py
+:language: py
+``` 
---
+++ source/properties/get-custom-metadata/py_usd_var1.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Dict, Any
+from pxr import Usd
+
+def get_customdata(usd: Usd.Object, key: Any = None) -> Any:
+    """
+    Args:
+        usd : Any USD scene description that has customData metadata.
+            For example, Usd.Prim, Usd.Property
+
+        key : Key to retrieve the value from the customData dictionary. 
+            If not provided, will return the entire metadata dictionary.
+    Return:
+        metadata : Python dictionary containing the metadata for the USD object, 
+            or the value associated with the key if provided.
+    """
+    if key:
+        return usd.GetCustomDataByKey(key)
+    else:
+        metadata : Dict = usd.GetCustomData()
+        return metadata
